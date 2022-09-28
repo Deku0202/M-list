774907541d45147a040b2f447c64da585592725e
Deku0202 <linmyathu195@gmail.com>
2022-09-29 00:40:34 +0630

    incorrect with only one html with if else

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def login():
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
-            return render_template("login-wrong.html")
+            return render_template("login.html", i=0)
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
